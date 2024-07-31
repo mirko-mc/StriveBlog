00938cb8b4b2deca7673606ab3b1fc7903fe7c5e
mirko-mc <8547740+mirko-mc@users.noreply.github.com>
2024-07-31 21:28:31 +0200

    preparazione form fetch in MyFetch.jsx

---
+++ striveblogfrontend/src/components/MyFetch.jsx
@@ -0,0 +1,12 @@
+import { Form } from "react-bootstrap";
+
+export const MyFetch = () => {
+  return (
+    <Form>
+      <Form.Group>
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text"></Form.Control>
+      </Form.Group>
+    </Form>
+  );
+};
--- striveblogfrontend/src/views/home/Home.jsx
+++ striveblogfrontend/src/views/home/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import BlogList from "../../components/blog/blog-list/BlogList";
 import "./styles.css";
+import { MyFetch } from "../../components/MyFetch";
 
-const Home = props => {
+const Home = (props) => {
   return (
     <Container fluid="sm">
       <h1 className="blog-main-title mb-3">Benvenuto sullo Strive Blog!</h1>
+      <MyFetch />
       <BlogList />
     </Container>
   );
