7b8a5b5dc4df4d83e9710e4aa9b0593bdd79816f
mirko-mc <8547740+mirko-mc@users.noreply.github.com>
2024-08-30 12:17:26 +0000

    FRONTEND added upload cover in newBlogPost

--- frontend/src/views/new/New.jsx
+++ frontend/src/views/new/New.jsx
@@ -5,7 +5,7 @@ import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import "./styles.css";
 import { convertToRaw } from "draft-js";
 import draftToHtml from "draftjs-to-html";
-import { PostNewBlogPost } from "../../data/fetch";
+import { PostNewBlogPost, PostPicture } from "../../data/fetch";
 
 const NewBlogPost = (props) => {
   const [text, setText] = useState("");
@@ -26,21 +26,35 @@ const NewBlogPost = (props) => {
   const [formValue, setFormValue] = useState(initialFormValue);
   /** fD conterrà l'immagine da passare alla fetch */
   const [fD, setFD] = useState(new FormData())
+  const handlePicture = (e) => {
+    setFD(prev => {
+      // !!! svuoto il formData
+      prev.delete("cover")
+      // !!! inserisco l'immagine nel formData
+      prev.append("cover", e.target.files[0])
+      return prev;
+    })
+  }
   const handleChangeFormValue = (e) => {
     setFormValue({ ...formValue, [e.target.name]: e.target.value });
   };
-  const handleSubmit = () => {
-Post
-    PostNewBlogPost(formValue);
+  const handleSubmit = async () => {
+    // !!! attendo che il blogPost venga salvato
+    const BlogPost = await PostNewBlogPost(formValue);
+    // !!! aggiungo la cover al post
+    await PostPicture("cover", BlogPost.id, fD)
+    // !!! restituisco il messaggio di blogPost salvato
+    alert("BlogPost salvato con successo")
+    // !!! ritorno alla home
   };
   return (
     <Container className="new-blog-container">
-      <Form onSubmit={() => handleSubmit()} className="mt-5">
+      <Form onSubmit={handleSubmit} className="mt-5">
 
 
         <Form.Group controlId="fileCover" className="mb-3">
-          <Form.Label>Cover</Form.Label>
-          <Form.Control type="file" />
+          <Form.Label>Immagine del post</Form.Label>
+          <Form.Control type="file" onChange={handlePicture}/>
         </Form.Group>
 
 
@@ -82,7 +96,7 @@ Post
             Reset
           </Button>
           <Button
-            // type="submit"
+            type="submit"
             size="lg"
             variant="dark"
             style={{
